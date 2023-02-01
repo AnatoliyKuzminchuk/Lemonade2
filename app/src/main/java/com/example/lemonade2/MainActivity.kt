a0d884c81d3d2f332c8349b59d828c5794246dde
Anatoliy <anatoliykuzminchuk@gmail.com>
2023-02-01 22:18:17 +0200

    Refactor

--- app/src/main/java/com/example/lemonade2/MainActivity.kt
+++ app/src/main/java/com/example/lemonade2/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.lemonade2.R
 import com.example.lemonade2.ui.theme.Lemonade2Theme
 
 
@@ -26,68 +25,106 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Lemonade2Theme {
-                LemonadeApp()
+                var result by remember { mutableStateOf(4) }
+
+                LemonadeProcess(
+                    result = result,
+                    onImageClick = {
+                        result = getNextInstruction(result)
+                    },
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center)
+                )
             }
         }
     }
 }
 
-@Preview
 @Composable
-fun LemonadeApp() {
-
-    LemonadeProcess(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center))
-}
-
-@Composable
-fun LemonadeProcess(modifier: Modifier = Modifier
+private fun LemonadeProcess(
+    modifier: Modifier = Modifier,
+    result: Int = 0,
+    onImageClick: () -> Unit
 ) {
-
-    var result by remember {
-        mutableStateOf(4)
-    }
-    val imageResource = when (result) {
-        1 -> R.drawable.lemon_squeeze
-        2 -> R.drawable.lemon_drink
-        3 -> R.drawable.lemon_restart
-        else -> R.drawable.lemon_tree
-    }
-    val StringResource = when (result) {
-        1 -> R.string.lemon
-        2 -> R.string.lemonade
-        3 -> R.string.glass
-        else -> R.string.lemon_tree
-    }
-    Surface(modifier = modifier,
-        color = MaterialTheme.colors.background) {
-        Column(modifier = modifier,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = stringResource(id = R.string.app_name), style = MaterialTheme.typography.h4)
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colors.background
+    ) {
+        Column(
+            modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.h4
+            )
             Spacer(modifier = Modifier.height(14.dp))
-            Text(text = stringResource(id = StringResource), style = MaterialTheme.typography.h6)
+
+            Text(
+                text = stringResource(
+                    id = when (result) {
+                        1 -> R.string.lemon
+                        2 -> R.string.lemonade
+                        3 -> R.string.glass
+                        else -> R.string.lemon_tree
+                    }
+                ),
+                style = MaterialTheme.typography.h6
+            )
+
             Spacer(modifier = Modifier.height(16.dp))
-            Image(painter = painterResource(id = imageResource), contentDescription = stringResource(id = StringResource),
+
+            Image(
+                painter = painterResource(
+                    id = when (result) {
+                        1 -> R.drawable.lemon_squeeze
+                        2 -> R.drawable.lemon_drink
+                        3 -> R.drawable.lemon_restart
+                        else -> R.drawable.lemon_tree
+                    }
+                ),
+                contentDescription = null,
                 modifier = Modifier
-                    .border(1.dp, MaterialTheme.colors.secondary, MaterialTheme.shapes.medium)
                     .fillMaxSize(fraction = 0.5f)
                     .wrapContentSize()
-                    .clickable {
-                        if (result == 1) {
-                            var temp = 1
-                            temp = (1..4).random()
-                            result = if (temp == 4) 2 else 1
-                        } else if (result == 2) {
-                            result = 3
-                        } else if (result == 3) {
-                            result = 4
-                        } else {
-                            result = 1
-                        }
-                    })
-
+                    //.clip(RoundedCornerShape(topEnd = 20.dp, bottomStart = 20.dp))
+                    .clickable(onClick = onImageClick)
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colors.secondary,
+                        shape = MaterialTheme.shapes.medium
+                    )
+            )
+        }
     }
+}
 
+private fun getNextInstruction(
+    result: Int
+) = when (result) {
+    1 -> {
+        val temp: Int = (1..4).random()
+        if (temp == 4) 2 else 1
+    }
+    2 -> 3
+    3 -> 4
+    else -> 1
+}
+
+@Preview
+@Composable
+private fun LemonadeApp() {
+    Lemonade2Theme {
+        var result by remember { mutableStateOf(4) }
+
+        LemonadeProcess(
+            result = result,
+            onImageClick = {
+                result = getNextInstruction(result)
+            },
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        )
     }
 }
